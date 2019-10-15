6c42c241dddac2d277a51ac8af6a465b6d300391
Alex Enache <ragnarxon@gmail.com>
2019-10-15 10:08:20 -0400

    landing page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import NavBar from './components/navbar';
+import HomePage from './components/homePage';
+import SubPage from './components/subPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <NavBar></NavBar>
+        <Switch>
+          <Route path='/' exact component={HomePage}/>
+          <Route path='/subpage' component={SubPage}/>
+        </Switch>
+      </Router>
     </div>
   );
 }
