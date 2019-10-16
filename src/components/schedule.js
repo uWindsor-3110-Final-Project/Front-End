d5281c0b2d914a05c422cb9c1b8a1542cef6bf94
taqif359 <taqiuddin.farooqui@gmail.com>
2019-10-16 12:05:14 -0400

    used map functions for the dropdown menus

--- src/components/schedule.js
+++ src/components/schedule.js
@@ -7,20 +7,38 @@ class schedule extends Component {
     super(props);
 
     this.state = {
-      startYear: "",
-      department: "",
-      major: "",
-      expectedGradYear: ""
+      value: "",
+      startYear: ["Select", "2018", "2019", "2020", "2021"],
+      department: ["Select", "Computer Science"],
+      major: [
+        "Select",
+        "Honours BCS w/ Software Engineering",
+        "Honours BCS w/ Artificial Intelligence",
+        "Honours BCS w/ Multimedia",
+        "Honours BCS w/ Security",
+        "Honours Bachelor of Applied Computing",
+        "Bachelor of Computer Science (General)",
+        "Computer Science for University Graduates"
+      ],
+      expectedGradYear: [
+        "Select",
+        "2020",
+        "2021",
+        "2022",
+        "2023",
+        "2024",
+        "2025"
+      ]
     };
 
-    this.handleChange = this.handleChange.bind(this);
+    // this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   //field handler
-  handleChange(event) {
+  handleChange = event => {
     this.setState({ value: event.target.value });
-  }
+  };
 
   handleSubmit(event) {
     alert(this.state.value);
@@ -29,80 +47,53 @@ class schedule extends Component {
 
   render() {
     return (
-      <div className="form-2">
+      <div className="form-for-info">
         <form onSubmit={this.handleSubmit}>
           <h3 className="form-header">Enter the following details:</h3>
           <br></br>
           <div>
             <label className="entity-label">
               University Start Year:
-              <select
-                className="menu"
-                value={this.state.value}
-                onChange={this.handleChange}
-              >
-                <option default>Select</option>
-                <option value="2018">2018</option>
-                <option value="2019">2019</option>
-                <option value="2020">2020</option>
-                <option value="2021">2021</option>
+              <select className="menu">
+                {this.state.startYear.map(list => (
+                  <option key={list} value={list}>
+                    {list}
+                  </option>
+                ))}
               </select>
             </label>
             <br></br>
 
             <label className="entity-label">
               Department:
-              <select
-                className="menu"
-                value={this.state.value}
-                onChange={this.handleChange}
-              >
-                <option default>Select</option>
-                <option value="Computer Science">Computer Science</option>
+              <select className="menu">
+                {this.state.department.map(list => (
+                  <option key={list} value={list}>
+                    {list}
+                  </option>
+                ))}
               </select>
             </label>
             <br></br>
             <label className="entity-label">
               Major:
-              <select
-                className="menu"
-                value={this.state.value}
-                onChange={this.handleChange}
-              >
-                <option default>Select</option>
-                <option value="softeng">
-                  Honours BCS w/ Software Engineering
-                </option>
-                <option value="ai">
-                  Honours BCS w/ Artificial Intelligence
-                </option>
-                <option value="multimedia">Honours BCS w/ Multimedia</option>
-                <option value="security">Honours BCS w/ Security</option>
-                <option value="applied computing">
-                  Honours Bachelor of Applied Computing
-                </option>
-                <option value="general">
-                  Bachelor of Computer Science (General)
-                </option>
-                <option value="unigrads">
-                  Computer Science for University Graduates
-                </option>
+              <select className="menu">
+                {this.state.major.map(list => (
+                  <option key={list} value={list}>
+                    {list}
+                  </option>
+                ))}
               </select>
             </label>
             <br></br>
             <label className="entity-label">
               Expected Graduation Year:
-              <select
-                className="menu"
-                value={this.state.value}
-                onChange={this.handleChange}
-              >
-                <option default>Select</option>
-                <option value="2020">2020</option>
-                <option value="2021">2021</option>
-                <option value="2022">2022</option>
-                <option value="2023">2023</option>
-                <option value="2024">2024</option>
+              <select className="menu">
+                {this.state.expectedGradYear.map(list => (
+                  <option key={list} value={list}>
+                    {list}
+                  </option>
+                ))}
               </select>
             </label>
             <br></br>
