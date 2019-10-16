f08d04a43f107b5a2911eccd96d20c13e3c31db0
taqif359 <taqiuddin.farooqui@gmail.com>
2019-10-16 10:44:56 -0400

    first commit of user info page

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import NavBar from './components/navbar';
+import HomePage from './components/homePage';
+import SubPage from './components/subPage';
+import Schedule from './components/schedule';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <NavBar></NavBar>
+        <Switch>
+          <Route exact path='/' component={HomePage}/>
+          <Route exact path='/subpage' component={SubPage}/>
+          <Route exact pUserInfoath='/schedule' component={Schedule}/>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
