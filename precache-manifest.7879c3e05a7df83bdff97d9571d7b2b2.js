43240dec80b608fe5d147181cde4a874957dcc9f
Jameel Jiwani <jameeljiwani@gmail.com>
2019-10-23 21:28:47 -0400

    Updates

---
+++ precache-manifest.7879c3e05a7df83bdff97d9571d7b2b2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b84b8f1a6844c6d85d490666d7501c94",
+    "url": "/Front-End/index.html"
+  },
+  {
+    "revision": "e23624db9936e4b8e0c0",
+    "url": "/Front-End/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "3473c690f1e5c3902c3f",
+    "url": "/Front-End/static/css/main.22335433.chunk.css"
+  },
+  {
+    "revision": "e23624db9936e4b8e0c0",
+    "url": "/Front-End/static/js/2.5d38ff13.chunk.js"
+  },
+  {
+    "revision": "3473c690f1e5c3902c3f",
+    "url": "/Front-End/static/js/main.2f5342db.chunk.js"
+  },
+  {
+    "revision": "d9e6119ca433820838d5",
+    "url": "/Front-End/static/js/runtime-main.8b7579fc.js"
+  },
+  {
+    "revision": "5e32e90db25fcb92c106fab8cd4e417c",
+    "url": "/Front-End/static/media/fullstackdads.5e32e90d.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Front-End/static/media/logo.25bf045c.svg"
+  }
+]);
